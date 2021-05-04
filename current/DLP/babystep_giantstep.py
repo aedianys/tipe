01e3508d98b3143099745eb9b6d45abc32ca4a29
Lunia Ayanides <lunia.ayanides@gmail.com>
2021-05-04 16:22:02 +0200

    Improve DLP

---
+++ current/DLP/babystep_giantstep.py
@@ -0,0 +1,31 @@
+#!/usr/bin/python3
+
+from math import ceil, sqrt
+
+def BSGS(a, b, G):
+    r"""Baby-step Giant-step algorithm for computing discrete logarithms
+    in a finite abelian group `G`.
+
+        INPUT:
+        - `a` must be a primitive element of `G`
+        - `b` must be an element of `G`
+
+        OUTPUT:
+        - `x` such that ``a^x = b``
+    """
+
+    dic = {}
+    m = ceil(sqrt(G.order))
+
+    aj = G.id
+    for j in range(m):
+        aj = G.op(aj,a)
+        dic[aj] = j
+    
+    c = G.exp(G.inv(a),m)
+    gam = b
+    for i in range(m):
+        if gam in dic:
+            return m*i+j
+        else:
+            gam = G.op(gam,c)
--- current/ECDLP/pohlig_hellman.py
+++ current/ECDLP/pohlig_hellman.py
@@ -1,51 +1,7 @@
 #!/usr/bin/sage
 
-from ..maths.math_lib import CRT
-from sage.all import factor, discrete_log
 from ..maths.groups import ECCGroup
-
-
-def PH(a, y, G):
-    r"""Pohlig-Hellman algorithm for computing discrete logarithms
-    in a finite abelian group `G` whose order is a smooth integer.
-
-        INPUT:
-        - `a` must be a primitive element of `G`
-        - `y` must be an element of `G`
-
-        OUTPUT:
-        - `x` such that ``a^x = y``
-    """
-
-    decomposition = factor(G.order)
-    residues = []
-    moduli = []
-    beta = G.inv(a)
-
-    for pi, ni in decomposition:
-        # compute the residue of x mod pi^ni
-        n = G.order // pi
-        yi = G.exp(a, n)
-        z = y
-        b = beta
-        power = 1
-        residue = 0
-
-        for j in range(ni):
-            # compute the j-th digit in base pi of x mod pi^ni
-            w = G.exp(z, n)
-            bj = discrete_log(w, yi, ord=pi, identity=G.id, inverse=G.inv, op=G.op)
-            residue += bj * power
-            z = G.op(z, G.exp(b, bj))
-            power *= pi
-            b = G.exp(b, pi)
-            n = n // pi
-
-        residues.append(residue)
-        moduli.append(power)
-
-    x = CRT(residues, moduli)
-    return x
+from ..DLP.pohlig_hellman import PH
 
 
 def ECC_PH(G, A, E):
