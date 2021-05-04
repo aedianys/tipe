0eaccdd456edbe17bd6a37babca6b1a36b781080
Lunia Ayanides <lunia.ayanides@gmail.com>
2021-05-04 16:22:55 +0200

    Add Pollard's Rho factorization

--- current/factor.py
+++ current/factor.py
@@ -1,11 +1,16 @@
 #!/usr/bin/python3
 
 from .IFP.ecm import ECM
+from .IFP.pollard_rho import pollard_rho
 
 def factor(n: int, alg="ECM"):
-    """Returns the factorization of `n` using Lenstra's factorization
-    algorithm on elliptic curves"""
+    """Returns the factorization of `n` using different algorithms :
+    - `'ECM'` -> Lenstra's factorization algorithm on elliptic curves
+    - `'pollard-rho'` -> Pollard's-rho algorithm with Brent's improvments
+    """
     if alg == "ECM":
         return ECM(n)
+    elif alg == "pollard-rho":
+        return pollard_rho(n)
     else:
         raise NotImplementedError
