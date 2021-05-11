6c7b9cf1957e8b122d53cad3498e09cea1bb6a90
Lunia Ayanides <lunia.ayanides@gmail.com>
2021-05-11 20:29:41 +0200

    Add naive point counting

---
+++ point_counting/naive.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+# -*- coding: utf-8 -*-
+
+from maths.math_lib import jacobi_symbol
+
+def naive_order(curve):
+    """Naive point counting for curve over Z/pZ"""
+    count = 1
+    p = curve.field.characteristic()
+
+    for x in range(p):
+        y2 = curve.eval(x)
+        j = jacobi_symbol(y2.remainder(), p)
+        if j == 0:
+            count += 1
+        elif j == 1:
+            count += 2
+    
+    return count
